0c0f37e546480863abd04e98cfb5978aecbc21c1
Chico <chicooliveira.com@gmail.com>
2025-04-06 06:18:05 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoagora/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppTempoagora
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
