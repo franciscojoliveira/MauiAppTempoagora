7502e147b51e95659b1cdd4f0218006b03774cf6
Chico <chicooliveira.com@gmail.com>
2025-04-06 14:10:09 -0300

    Tempo Agora

--- MauiAppTempoagora/MainPage.xaml.cs
+++ MauiAppTempoagora/MainPage.xaml.cs
@@ -1,24 +1,60 @@
-﻿namespace MauiAppTempoagora
+﻿using MauiAppTempoagora.Models;
+using MauiAppTempoagora.Services;
+
+namespace MauiAppTempoagora
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
-
+       
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+       
+
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if (t != null)
+                    {
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat}\n" +
+                                         $"Longitude: {t.lon}\n" +
+                                         $"Nascer do Sol: {t.sunrise}\n" +
+                                         $"Pôr do Sol: {t.sunset}\n" +
+                                         $"Temperatura Máxima: {t.temp_max}°C\n" +
+                                         $"Temperatura Mínima: {t.temp_min}°C\n" +
+                                         $"Velocidade do Vento: {t.speed} m/s\n" +
+                                         $"Visibilidade: {t.visibility} metros\n" +
+                                         $"Descrição: {t.description}\n";
+
+                        lbl_res.Text = dados_previsao;
+
+
+                    } else
+                    {
+                        lbl_res.Text = "Sem dados de Previsão";
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Prencha a cidade.";
+                    
+                }
+
+            } catch(Exception ex) 
+            {
+                await DisplayAlert("Ops", ex.Message, "Ok");
+            }
         }
     }
 
---
+++ MauiAppTempoagora/Services/DataService.cs
@@ -0,0 +1,61 @@
+﻿using MauiAppTempoagora.Models;
+using Newtonsoft.Json.Linq;
+
+
+namespace MauiAppTempoagora.Services
+
+{
+    public class DataService
+
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+
+        {
+            Tempo? t = null;
+
+            string chave = "6cfcebf9fa9fdd860dba7ce24d165039";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString("HH:mm:ss"),
+                        sunset = sunset.ToString("HH:mm:ss"),
+
+                    }; // Fecha obj do Tempo.
+                } // Feha if se o status do servidor foi de sucesso
+            } // Fecha laça using
+
+
+            return t;
+
+        }
+
+        internal static async Task<Tempo?> GetPrevisao(object txt_cidade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
